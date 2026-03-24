c390aad560e419b662d4aa639e45f727a23ab5c9
sagar-pardhi <sgrpardhi007@gmail.com>
2026-03-24 21:53:14 +0530

    feat: implement project management features with authentication and UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useMutation, useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
+import { ProjectsView } from "@/features/projects/components/projects-view";
 
 export default function Home() {
-  const tasks = useQuery(api.projects.get);
-  const createProject = useMutation(api.projects.create);
-
-  return (
-    <main className="flex flex-col gap-2 p-4">
-      <Button
-        onClick={() => {
-          createProject({ name: "New Project" });
-        }}
-      >
-        Add New
-      </Button>
-
-      {tasks?.map(({ _id, ownerId, name }) => (
-        <div key={_id} className="border rounded p-2 flex flex-col">
-          <p>{name}</p>
-          <p>Owner Id: {ownerId}</p>
-        </div>
-      ))}
-    </main>
-  );
+  return <ProjectsView />;
 }
